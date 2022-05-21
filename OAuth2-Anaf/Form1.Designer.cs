409874cb544207cd2bdaa5fe92e61c7dbb14d79e
LucianR <LucianR@DESKM>
2022-05-21 23:40:53 +0300

    Add project files.

---
+++ OAuth2-Anaf/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace OAuth2_Anaf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.edB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edCUI = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.edClientID = new System.Windows.Forms.TextBox();
+            this.edSecret = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.edCallBack = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(208, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Lista facturi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(524, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 51);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Obtine cod Auth";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 95);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(657, 122);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Bearer: ";
+            // 
+            // edB
+            // 
+            this.edB.Location = new System.Drawing.Point(71, 34);
+            this.edB.Name = "edB";
+            this.edB.Size = new System.Drawing.Size(434, 20);
+            this.edB.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "CUI";
+            // 
+            // edCUI
+            // 
+            this.edCUI.Location = new System.Drawing.Point(71, 66);
+            this.edCUI.Name = "edCUI";
+            this.edCUI.Size = new System.Drawing.Size(104, 20);
+            this.edCUI.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "ClientID";
+            // 
+            // edClientID
+            // 
+            this.edClientID.Location = new System.Drawing.Point(98, 23);
+            this.edClientID.Name = "edClientID";
+            this.edClientID.Size = new System.Drawing.Size(400, 20);
+            this.edClientID.TabIndex = 10;
+            // 
+            // edSecret
+            // 
+            this.edSecret.Location = new System.Drawing.Point(98, 60);
+            this.edSecret.Name = "edSecret";
+            this.edSecret.Size = new System.Drawing.Size(400, 20);
+            this.edSecret.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "SecretID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "CallBack URL";
+            // 
+            // edCallBack
+            // 
+            this.edCallBack.Location = new System.Drawing.Point(98, 90);
+            this.edCallBack.Name = "edCallBack";
+            this.edCallBack.Size = new System.Drawing.Size(400, 20);
+            this.edCallBack.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 411);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(341, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Demo obtinere token autorizare si lista facturi de pe ANAF, prin OAuth2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 428);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(287, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Autor Lucian Rosca, Medialogic SRL, office@medialogic.ro";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(521, 26);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(171, 26);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Pentru obtinere cod, e necesar  ca\r\ntokenul de semnatura sa fie in PC";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.edSecret);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.edClientID);
+            this.groupBox1.Controls.Add(this.edCallBack);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(32, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(713, 145);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Obtinere token";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.edB);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.edCUI);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(32, 165);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(713, 211);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Preluare facturi din SPV api";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(101, 113);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(408, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "(trebuie sa fie exact cum l-ati definit cand ati inregistrat aplicatia in API-Oau" +
+    "th la ANAF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Name = "Form1";
+            this.Text = "Demo OAuth2 - ANAF - eFactura";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox edB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox edCUI;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox edClientID;
+        private System.Windows.Forms.TextBox edSecret;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox edCallBack;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ OAuth2-Anaf/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace OAuth2_Anaf
+{
+    public partial class Form1 : Form
+    {
+        string code = "";
+        string URL_auth = "https://logincert.anaf.ro/anaf-oauth2/v1/authorize";
+        string URL_getToken = "https://logincert.anaf.ro/anaf-oauth2/v1/token";
+
+        
+        Token token = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+      
+
+          
+        }
+
+  
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // preia token
+            this.Validate();
+            if (String.IsNullOrEmpty(edClientID.Text))
+                throw new Exception("Lipseste ClientID");
+            if (String.IsNullOrEmpty(edSecret.Text))
+                throw new Exception("Lipseste SecretID");
+            string u = HttpUtility.UrlEncode(edCallBack.Text);
+            string url = string.Format("{0}?response_type=code&client_id={1}&redirect_uri={2}",
+    URL_auth,
+    edClientID.Text,
+    u);
+            
+
+
+
+            var handler = new WebRequestHandler();
+            handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+            var cert = getCert();
+            handler.ClientCertificates.Add(cert);
+            handler.UseProxy = false;
+            HttpClient client = new HttpClient(handler);
+
+            var resultString = "";
+            HttpResponseMessage result = null;
+            try
+            {
+                result = client.GetAsync(url).GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            if (result.RequestMessage.RequestUri.Query.Contains("code="))
+            {
+                code = result.RequestMessage.RequestUri.Query.Replace("?code=", "");
+                var par = string.Format("grant_type=authorization_code&code={0}&client_id={1}&client_secret={2}&redirect_uri={3}",
+code,
+edClientID.Text,
+edSecret.Text,
+u);
+                try
+                {
+                    result = client.PostAsync(URL_getToken, new StringContent(par)).GetAwaiter().GetResult();
+                    resultString = result.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    if (resultString.Contains("access_token"))
+                    {
+                        var js = new JavaScriptSerializer();
+                        token = js.Deserialize<Token>(resultString);
+                        edB.Text = token.access_token;
+                        MessageBox.Show("Tokenul a fost preluat.");
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Tokenul NU a fost preluat. Rezultat primit: "+resultString);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+                
+
+   
+
+
+
+        }
+        public static X509Certificate2 getCert(bool toate = false)
+        {
+            X509Store store = new X509Store("MY", StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+
+            X509Certificate2Collection collection = (X509Certificate2Collection)store.Certificates;
+            X509Certificate2Collection fcollection = (X509Certificate2Collection)collection.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
+
+            X509Certificate2Collection HardCollection = new X509Certificate2Collection();
+            string sn = "";
+
+
+            foreach (var x5 in fcollection)
+            {
+                if ((toate) || ((x5.IssuerName.Name.ToUpper().Contains("CERTSIGN")) || (x5.IssuerName.Name.ToUpper().Contains("DIGISIGN")) || (x5.IssuerName.Name.ToUpper().Contains("TRAN")) || (x5.IssuerName.Name.ToUpper().Contains("ALFASIGN")) || (x5.IssuerName.Name.ToUpper().Contains("CERT DIGI")) || (x5.IssuerName.Name.ToUpper().Contains("CERTDIGITAL")) || (x5.IssuerName.Name.ToUpper().Contains("DE CALCUL"))))
+                {
+                    if ((String.IsNullOrEmpty(sn) || (x5.SerialNumber == sn)))
+                        HardCollection.Add(x5);
+                }
+            }
+            if (HardCollection.Count > 1)
+            {
+                X509Certificate2Collection scollection = X509Certificate2UI.SelectFromCollection(HardCollection, "Alege certificatul", "Select a certificate from the following list to get information on that certificate", X509SelectionFlag.SingleSelection);
+                store.Close();
+                return scollection[0];
+            }
+            else
+            {
+                if (HardCollection.Count == 1)
+                {
+                    store.Close();
+                    return HardCollection[0];
+                }
+                else
+                {
+                    store.Close();
+                    return null;
+                }
+
+            }
+
+
+
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // preia facturi
+            this.Validate();
+            if (String.IsNullOrEmpty(edCUI.Text))
+            {
+                throw new Exception("Lipseste codul fiscal");
+            }
+
+            if (String.IsNullOrEmpty(edB.Text))
+            {
+                throw new Exception("Lipseste tokenul");
+            }
+            string URL_getFacturi = "https://api.anaf.ro/prod/FCTEL/rest/listaMesajeFactura?zile=60&cif="+edCUI.Text;
+            var handler = new WebRequestHandler();
+
+            handler.UseProxy = false;
+            HttpClient client = new HttpClient(handler);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", edB.Text);
+
+            var ListaFacturiJSON = "";
+            HttpResponseMessage result = null;
+            try
+            {
+                result = client.GetAsync(URL_getFacturi).GetAwaiter().GetResult();
+                ListaFacturiJSON = result.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                if (result.StatusCode != HttpStatusCode.OK)
+                    textBox1.Text = result.ReasonPhrase;
+                else
+                    textBox1.Text = ListaFacturiJSON;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ceva a mers naspa :) "+ex.Message);
+            }
+        }
+    }
+
+        
+}
